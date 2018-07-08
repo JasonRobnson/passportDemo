c8a6cf2fbd2cbf4c3fc4f398c30486c3bf196c20
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-08 11:51:52 -0500

    modified to es6

--- mern-secure/models/User.js
+++ mern-secure/models/User.js
@@ -17,11 +17,11 @@ const UserSchema = new Schema({
 UserSchema.pre('save', function(next) {
   var user = this;
   if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.genSalt(10, (err, salt) => {
       if (err) {
         return next(err);
       }
-      bcrypt.hash(user.password, salt, null, function(err, hash) {
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
         if (err) {
           return next(err);
         }
@@ -35,7 +35,7 @@ UserSchema.pre('save', function(next) {
 });
 
 UserSchema.methods.comparePassword = function(passw, cb) {
-  bcrypt.compare(passw, this.password, function(err, isMatch) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
     if (err) {
       return cb(err);
     }

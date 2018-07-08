e01c672632e268a2e31914dbf3971e67d4a2da22
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-07 19:09:25 -0500

    refactored

--- mern-secure/routes/book.js
+++ mern-secure/routes/book.js
@@ -1,48 +1,47 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const Book = require('../models/Book.js');
-const passport = require('passport');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Book = require('../models/Book.js');
+var passport = require('passport');
 require('../config/passport')(passport);
 
 /* GET ALL BOOKS */
-router.get('/', passport.authenticate('jwt', { session: false }), function (req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        Book.find(function (err, books) {
-            if (err) return next(err);
-            res.json(books);
-        });
-    } else {
-        return res.status(403).send({ success: false, msg: 'Unauthorized.' });
-    }
+router.get('/', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    Book.find(function (err, books) {
+      if (err) return next(err);
+      res.json(books);
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'});
+  }
 });
 
 /* SAVE BOOK */
-router.post('/', passport.authenticate('jwt', { session: false }), function (req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        Book.create(req.body, function (err, post) {
-            if (err) return next(err);
-            res.json(post);
-        });
-    } else {
-        return res.status(403).send({ success: false, msg: 'Unauthorized.' });
-    }
+router.post('/', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    Book.create(req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'});
+  }
 });
 
 getToken = function (headers) {
-    if (headers && headers.authorization) {
-        var parted = headers.authorization.split(' ');
-        if (parted.length === 2) {
-            return parted[1];
-        } else {
-            return null;
-        }
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
     } else {
-        return null;
+      return null;
     }
+  } else {
+    return null;
+  }
 };
-
 
 module.exports = router;
